e001ad287f292fc1d04e0fe72141f7c4f314753b
Brian <91170771+bg-cs@users.noreply.github.com>
2023-03-16 08:59:21 -0700

    adjustments

--- dashboard/index.php
+++ dashboard/index.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dashboard</title>
 
+        <!--jQuery 3.6.0-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <!--Bootstrap 3.3.7-->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
         <script src="js/bootstrap.min.js"></script>
@@ -15,16 +17,17 @@
         <link rel="stylesheet" type="text/css" href="css/css-app.css">
         <!--Lato and Zilla Slab fonts-->
         <link href=""href="https://fonts.googleapis.com/css?family=Lato|Zilla+Slab&display=swap" rel="stylesheet">
-        <!--jQuery 3.6.0-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        
         <!--DataTables 1.13.2-->
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.css">
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.js"></script>
 
         <style>
             h1 {
+                
                 font-family: 'Lato';
+            }
+            h2 {
+                margin-top: 16px;
             }
             h3 {
                 font-family: 'Lato';
@@ -40,59 +43,87 @@
             .hidden {
                 display: none !important;
             }
-            .row {
-                margin-top: 15px;
-                margin-bottom: 15px;
-	            margin-left: 30px;
-	            margin-right: 30px
+            .main-top-bar {
+                /*background-color: #777;*/
+            }
+            .middle-top-bar {
+                background-color: #D3D2D2;
+                margin-bottom: 30px;
+            }
+            .top-bar {
+                margin-left: 100px;
+                margin-right: 100px;
+            }
+            :root {
+                background-color: #EDEAE8;
             }
         </style>
     </head>
 
     <body class="ab-xl ab-desktop">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 col-md-9">
-                    <h1 id="page-title">Dashboard</h1>
-                </div>
-                <div class="col-12 col-md-3">
-                    <button class="btn btn-danger btn-block btn-h1 active" id="fadebutton">
-                        <span class="glyphicon glyphicon-share" aria-hidden="true"></span> Export data
-                    </button>
-                </div>
-            </div>
+        <div class="main-content">
 
-            <div class="d-flex alert alert-dismissible alert-success hidden" id="fademe">
-                <div class="alert-brand">
-                </div>
-                <div class="alert-content px-3">
-                    File automatically downloading...
-                </div>
-                <div class="alert-close">
-                    <button aria-label="Close" class="close" data-dismiss="alert" type="button">×</button>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="col-sm-4 abstract-item center-block border-right">
-                        <h2 class="text-center">Popular Tip</h2>
-                        <h3 class="text-center">Change your air filter</h3>
-                        <p class="text-center">(As of 03/16/2023 00:00:00)</p>
+            <div class="main-top-bar">
+                <div class="row top-bar">
+                    <div class="col-12 col-md-9">
+                        <h1 id="page-title">
+                            <span class="glyphicon glyphicon-console" aria-hidden="true"></span> Dashboard
+                        </h1>
                     </div>
-                    <div class="col-sm-4 abstract-item center-block border-right border-left">
-                        <h2 class="text-center">Most used browser</h2>
-                        <h3 class="text-center">Google Chrome</h3>
-                        <p class="text-center">(As of 03/16/2023 00:00:00)</p>
-                    </div>
-                    <div class="col-sm-4 abstract-item center-block border-left">
-                        <h2 class="text-center">Total Pageviews</h2>
-                        <h3 class="text-center">500,000,000,000</h3>
-                        <p class="text-center">(As of 03/16/2023 00:00:00)</p>
+                    <div class="col-12 col-md-3">
+                        <button class="btn btn-danger btn-block btn-h1 active" id="fadebutton">
+                            <span class="glyphicon glyphicon-open" aria-hidden="true"></span> Export data
+                        </button>
                     </div>
                 </div>
             </div>
 
+            <div class="section">
+                <div class="section-body">
+                    <div class="container">
+                        <div class="d-flex alert alert-dismissible alert-success hidden" id="fademe">
+                            <div class="alert-brand">
+                            </div>
+                            <div class="alert-content px-3">
+                                <span class="glyphicon glyphicon-saved" aria-hidden="true"></span> File automatically downloading...
+                            </div>
+                            <div class="alert-close">
+                                <button aria-label="Close" class="close" data-dismiss="alert" type="button">x</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section middle-top-bar">
+                <div class="section-body">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="col-sm-4 abstract-item center-block border-right">
+                                    <h2 class="text-center">Popular Tip</h2>
+                                    <h3 class="text-center">Change your air filter</h3>
+                                    <p class="text-center">(As of 03/16/2023 00:00:00)</p>
+                                </div>
+                                <div class="col-sm-4 abstract-item center-block border-right border-left">
+                                    <h2 class="text-center">Most used browser</h2>
+                                    <h3 class="text-center">Google Chrome</h3>
+                                    <p class="text-center">(As of 03/16/2023 00:00:00)</p>
+                                </div>
+                                <div class="col-sm-4 abstract-item center-block border-left">
+                                    <h2 class="text-center">Total Pageviews</h2>
+                                    <h3 class="text-center">500,000,000,000</h3>
+                                    <p class="text-center">(As of 03/16/2023 00:00:00)</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section">
+                <div class="section-body">
+                    <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="panel panel-default">
@@ -103,7 +134,13 @@
                     </div>
                 </div>
             </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="section">
+                <div class="section-body">
+                    <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="panel panel-default">
@@ -113,13 +150,10 @@
                     </div>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    
-                    <h4 id = "fademe">File automatically downloading... </h4>
+                    </div>
                 </div>
             </div>
+
         </div>
 
         <script>
