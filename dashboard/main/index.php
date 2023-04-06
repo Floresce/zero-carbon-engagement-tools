0f4f0bfd2c522a59c9e331cf1593078480958a7d
Brian <91170771+bg-cs@users.noreply.github.com>
2023-04-06 03:59:55 -0700

    cosmetic stuff and fonts

--- dashboard/main/index.php
+++ dashboard/main/index.php
@@ -20,18 +20,18 @@
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div class="container-fluid">
 
-                <div class="row d-flex align-items-center">
-                    <div class="col-9 d-flex flex-row p-3">
+                <div class="row d-flex align-items-center border-bottom my-3">
+                    <div class="col-9 d-flex flex-row ">
                         <h1>Dashboard</h1>
                     </div>
-                    <div class="col-3 d-flex flex-row-reverse p-3">
+                    <div class="col-3 d-flex flex-row-reverse ">
                         <button class="btn btn-danger" id="fadebutton">
                             <span class="glyphicon glyphicon-open" aria-hidden="true"></span> Export data
                         </button>
                     </div>
                 </div>
 
-                <div class="row middle-top-bar">
+                <div class="row middle-top-bar border shadow">
                     <div class="col p-5 border-end">
                         <h2 class="text-center">Popular Tip</h2>
                         <h3 class="text-center">Change your air filter</h3>
--- dashboard/main/php/tips_telemetry.php
+++ dashboard/main/php/tips_telemetry.php
@@ -1,4 +1,4 @@
-<div class="table-responsive p-4">
+<div class="table-responsive border p-4 my-4 shadow">
     <h3>Tips Report Telemetry</h3>
     <table class="table">
         <tbody>
