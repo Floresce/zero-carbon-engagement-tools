af7f7de9f6eaa27c50bdc55f307389a66b0a87ae
Girard Lin <girardlin@gmail.com>
2023-04-07 15:11:42 -0700

    Tips.php Styling
    Reenabled bootstrap icons for trash icon.

--- tips.php
+++ tips.php
@@ -508,6 +508,7 @@ if (count($rows) > 0) {
                       </form> <br>';
         }
 
+        echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">';
         // Add button to add to plan
         $result3 = '<div id="atpDiv-' . $tipId . '">';
         $result3 .= '<a href="#" onclick="return false;" class="btn btn-atp atpBtn" id="atpBtn-' . $tipId . '">Add to Plan</a>';
