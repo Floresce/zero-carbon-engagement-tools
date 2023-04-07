8dbd3fee10185084ea6f4d705b760eca1b48df9b
Brian <91170771+bg-cs@users.noreply.github.com>
2023-04-07 12:36:27 -0700

    minor fix 2

--- dashboard/DashBoardTipManipulation/php/database.php
+++ dashboard/DashBoardTipManipulation/php/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "mssql";        // Subject to change depending on server name (Anne: DESKTOP-UK8K0FD, Leo: MineHarth)
+$servername = "BOOKPRO";        // Subject to change depending on server name (Anne: DESKTOP-UK8K0FD, Leo: MineHarth)
 $database = "master";                     // Subject to change depending on database name
-$username = "sa";
-$password = "Programmadelic_123";
+$username = "admin";
+$password = "admin";
 
 $connectionInfo = array(
     "Database" => $database,
