9fe010026ae1f16b93562e67ce569feff092b366
Brian <91170771+bg-cs@users.noreply.github.com>
2023-05-07 21:13:35 -0700

    changes

--- dashboard/main/php/tips_telemetry_fetch.php
+++ dashboard/main/php/tips_telemetry_fetch.php
@@ -46,20 +46,7 @@ foreach ($tips_data as $row) {
     $comment_result = sqlsrv_query($conn, $comment_query);
     $comment_row = sqlsrv_fetch_array($comment_result);
     $COMMENT_COUNTS = $comment_row["count"];
-    $row["COMMENT_COUNT"] = "<a href='#' id='myLink". $T_ID ."'>" . $COMMENT_COUNTS . "</a>
-    <script>
-        $('#myLink" . $T_ID ."').on('click', function(event) {
-            var url = 'php/tips_comments.php?id=' + " . $T_ID .";
-            $.ajax({
-                url: url,
-                method: 'POST',
-                success: function (response) {
-                    $('#comment-content').html(response);
-                }
-            });
-        });
-    </script>
-    ";
+    $row["COMMENT_COUNT"] = $COMMENT_COUNTS;
 
     $data[] = $row;
 }
