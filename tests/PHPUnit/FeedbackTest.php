0e1d4ed734f5fc6c0768a589c4d34029c8bfff95
cyber-mary <angeliquemendoza@csus.edu>
2023-05-09 15:41:33 -0700

    fixed calling functions

--- tests/PHPUnit/FeedbackTest.php
+++ tests/PHPUnit/FeedbackTest.php
@@ -27,8 +27,8 @@ class FeedbackTest extends TestCase
     {
          //using tip 2 for testing purposes
          $tid = 2;
-         $likecount = tips::getLikes($tid);
-         tips::addLike($tid);
+         $likecount = getLikes($tid);
+         addFeeback($tid, TRUE);
          $updatedcount = tips::getLikes($tid);
          $this -> assertTrue($updatedcount == $likecount + 1);
 
@@ -39,9 +39,9 @@ class FeedbackTest extends TestCase
     {
         //using tip 2 for testing purposes
         $tid = 2;
-        $dislikecount = tips::getDislikes($tid);
-        tips::addLike($tid);
-        $updatedcount = tips::getDislikes($tid);
+        $dislikecount = getDislikes($tid);
+        addFeeback($tid, FALSE);
+        $updatedcount = getDislikes($tid);
         $this -> assertTrue($updatedcount == $dislikecount + 1);
 
     }
